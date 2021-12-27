4813caf675d7e5a739f777fffc9c89f7fdfa69c2
Rani DilipKumar Shiva Shankar <shankaarshiva@gmail.com>
2021-12-28 00:31:05 +0800

    minor updates

---
+++ leetcode/1-Two-Sums/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target: int):
+        d = {}
+        for i in range(len(nums)):
+            j = target - nums[i]
+            if j in d:
+                return [d[j], i]
+            else:
+                d[nums[i]] = i
+
+
+s = Solution()
+print(s.twoSum(
+    [3, 2, 4], 6))
