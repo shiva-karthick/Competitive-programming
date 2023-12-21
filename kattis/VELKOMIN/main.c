a58f7506c1c72fac7387d8795f668e150185c45e
shankar-shiv <shankaarshiva@gmail.com>
2023-12-22 01:49:37 +0800

    cleaned up files; restarted on kattis minimum 10 a day

---
+++ kattis/VELKOMIN/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char**argv){
+    char *str = "VELKOMIN!";
+    printf("%s", str);
+    return 0;
+}
---
+++ kattis/a-leid-i-bio/main.c
@@ -0,0 +1,7 @@
+#include "/home/shankar/Shiva/Competitive-programming/header-files/common.h"
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
